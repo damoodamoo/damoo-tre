f6e2da96ceace7e60a6419fa497e255ebeae8453
David Moore <damoo@microsoft.com>
2023-02-03 16:00:31 +0000

    sha

---
+++ .github/scripts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# set -o xtrace
+
+echo "HIIIIIII"
