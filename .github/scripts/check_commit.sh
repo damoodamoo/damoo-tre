400820f18b440e22d5089d7fb407c50ef1d15f75
David Moore <damoo@microsoft.com>
2023-02-03 18:52:23 +0000

    sha

--- .github/scripts/check_commit.sh
+++ .github/scripts/check_commit.sh
@@ -5,32 +5,16 @@ set -o nounset
 # Uncomment this line to see each command for debugging (careful: this will show secrets!)
 #set -o xtrace
 
-echo "8"
 COMMIT_SHA="${1}"
 ENVIRONMENT="${2}"
-echo "11"
 
-git --version
-echo "14"
+SHA_IN_ENV_BRANCH=$(git branch -r --contains "${COMMIT_SHA}" | grep -w "origin/${ENVIRONMENT}")
 
-git config user.name "GitHub Actions Bot"
-git config user.email "<>"
+echo "${SHA_IN_ENV_BRANCH}"
 
-git fetch
-
-git branch --contains 5bb71c81894811bd9aac0914b0999ae9fb77b1ab
-
-SHA_IN_ENV_BRANCH=$(git branch --contains "${COMMIT_SHA}")
-
-grep --version
-echo "24"
-SHA_IN_ENV_BRANCH=$(git branch --contains "${COMMIT_SHA}" | grep -w "${ENVIRONMENT}")
-echo "18"
-# echo "${SHA_IN_ENV_BRANCH}"
-
-# if [[ -z "${SHA_IN_ENV_BRANCH}" ]] ; then
-#   echo "Commit is not in ${ENVIRONMENT} branch. Cannot deploy."
-#   exit 1
-# fi
+if [[ -z "${SHA_IN_ENV_BRANCH}" ]] ; then
+  echo "Commit is not in ${ENVIRONMENT} branch. Cannot deploy."
+  exit 1
+fi
 
 # echo "Commit ${COMMIT_SHA} found in ${ENVIRONMENT} branch. Continuing."
